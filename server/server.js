f768662f4c708ab09c04205570cab7cb3ab7be6c
Satish Shriwas <satish@example.com>
2025-09-29 22:49:47 +0530

    Revert "chore(server): update server.js"
    This reverts commit e94a247a4c70f1c683e559628d347fa201889e74.

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
   methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
   credentials: true
 }));
