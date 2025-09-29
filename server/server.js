2ae386892561e2d90df5548c19cfeb18c774ffc3
Satish Shriwas <satish@example.com>
2025-09-29 23:32:52 +0530

    Fix: CORS Error

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://primetrade-ai1-2.onrender.com'],
   methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
   credentials: true
 }));
